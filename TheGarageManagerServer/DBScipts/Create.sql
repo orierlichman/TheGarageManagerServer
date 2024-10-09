57d40062bf6046037ab73db0cfcd6d7d9e743d5e
User <User@LAB3-3>
2024-10-09 09:28:15 +0300

    added sql, not finish and need to add number of NVARCHAR

--- TheGarageManagerServer/DBScipts/Create.sql
+++ TheGarageManagerServer/DBScipts/Create.sql
@@ -9,19 +9,101 @@ Create Database TheGarageManagerDB
 Go
 Use TheGarageManagerDB
 Go
-Create Table AppUsers
-(
-	Id int Primary Key Identity,
-	UserName nvarchar(50) Not Null,
-	UserLastName nvarchar(50) Not Null,
-	UserEmail nvarchar(50) Unique Not Null,
-	UserPassword nvarchar(50) Not Null,
-	IsManager bit Not Null Default 0
-)
-Insert Into AppUsers Values('admin', 'admin', 'kuku@kuku.com', '1234', 1)
+
+CREATE TABLE GARAGE (
+    GarageID NVARCHAR PRIMARY KEY,
+    MosahNumber INT,
+    GarageName NVARCHAR,
+    TypeCode INT,
+    GarageType NVARCHAR,
+    GarageAddress NVARCHAR,
+    City NVARCHAR,
+    Phone INT,
+    ZipCode INT,
+    SpecializationCode INT,
+    Specialization NVARCHAR,
+    ManagerSpecialization NVARCHAR,
+    License INT,
+    TestTime DATE,
+    WorkingHours VARCHAR
+);
+
+CREATE TABLE VEHICLE (
+    LicensePlate INT PRIMARY KEY,
+    Model NVARCHAR,
+    YearVehicle INT,
+    FuelType NVARCHAR,
+    Color NVARCHAR,
+    Manufacturer NVARCHAR,
+    CurrentMileage INT
+);
+
+CREATE TABLE CARREPAIR (
+    RepairID INT PRIMARY KEY,
+    LicensePlate INT,
+    GarageID INT,
+    RepairDate DATETIME,
+    DescriptionCar TEXT,
+    Cost INT,
+    FOREIGN KEY (LicensePlate) REFERENCES VEHICLE(LicensePlate),
+    FOREIGN KEY (GarageID) REFERENCES GARAGE(GarageID)
+);
+
+CREATE TABLE VEHICLEPART (
+    PartID INT PRIMARY KEY,
+    PartName NVARCHAR,
+    PartNumber INT,
+    Cost INT,
+    Supplier NVARCHAR,
+    ImageURL NVARCHAR
+);
+
+CREATE TABLE GARAGE_PARTS (
+    GaragePartID INT PRIMARY KEY,
+    GarageID INT,
+    PartID INT,
+    QuantityAvailable INT,
+    FOREIGN KEY (GarageID) REFERENCES GARAGE(GarageID),
+    FOREIGN KEY (PartID) REFERENCES VEHICLEPART(PartID)
+);
+
+CREATE TABLE EMPLOYEE (
+    EmployeeID INT PRIMARY KEY,
+    NameEmployee NVARCHAR,
+    Position NVARCHAR,
+    GarageID INT,
+    Phone INT,
+    Email NVARCHAR,
+    HireDate DATE,
+    Specialization NVARCHAR,
+    FOREIGN KEY (GarageID) REFERENCES GARAGE(GarageID)
+);
+
+CREATE TABLE REPAIRIMAGE (
+    ImageID INT PRIMARY KEY,
+    RepairID INT,
+    ImageURL NVARCHAR,
+    UploadDate DATETIME,
+    FOREIGN KEY (RepairID) REFERENCES CARREPAIR(RepairID)
+);
+
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY IDENTITY,
+    ManagerName NVARCHAR,
+    License INT,
+    UserStatusID INT,
+    FOREIGN KEY (UserStatusID) REFERENCES UserStatus(StatusID)
+);
+
+CREATE TABLE UserStatus (
+    StatusID INT PRIMARY KEY,
+    StatusName NVARCHAR
+);
+
+Insert Into Users Values('admin', 23345567, 1)
 Go
 -- Create a login for the admin user
-CREATE LOGIN [TheGarageManagerAdminLogin] WITH PASSWORD = 'thePassword';
+CREATE LOGIN [TheGarageManagerAdminLogin] WITH PASSWORD = 'admin123';
 Go
 
 -- Create a user in the YourProjectNameDB database for the login
