4cfaa9f09234c51d74c8e1cf50169c0a47b9ee5b
User <User@LAB3-3>
2024-11-05 12:59:26 +0200

    did AppointmentDTO

--- TheGarageManagerServer/DTO/AppointmentDTO.cs
+++ TheGarageManagerServer/DTO/AppointmentDTO.cs
@@ -3,10 +3,40 @@
     public class AppointmentDTO
     {
         public int AppointmentID { get; set; }
-        public DateTime AppointmentDate { get; set; }
-        public int GarageID { get; set; }
-        public int LicensePlate { get; set; }
-        public string AppointmentStatus { get; set; }
-        public DateTime ConfirmDate { get; set; }
+        public DateTime? AppointmentDate { get; set; }
+        public int? GarageID { get; set; }
+        public int? LicensePlate { get; set; }
+        public string? AppointmentStatus { get; set; }
+        public DateTime? ConfirmDate { get; set; }
+
+
+        public AppointmentDTO() { } 
+
+        public AppointmentDTO(Models.Appointment modelStudent)
+        {
+            this.AppointmentID = modelStudent.AppointmentId;
+            this.AppointmentDate = modelStudent.AppointmentDate;
+            this.GarageID = modelStudent.GarageId;
+            this.LicensePlate = modelStudent.LicensePlate;
+            this.AppointmentStatus = modelStudent.AppointmentStatus;
+            this.ConfirmDate = modelStudent.ConfirmDate;
+
+        }
+
+
+        public Models.Appointment GetModels()
+        {
+            Models.Appointment modelsUser = new Models.Appointment()
+            {
+                AppointmentId = this.AppointmentID,
+                AppointmentDate = this.AppointmentDate,
+                GarageId = this.GarageID,
+                LicensePlate = this.LicensePlate,
+                AppointmentStatus = this.AppointmentStatus,
+                ConfirmDate = this.ConfirmDate,
+            };
+            return modelsUser;
+        }
+
     }
 }
