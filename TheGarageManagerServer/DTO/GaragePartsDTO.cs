98abd6bcc92389230fed5689a2691509dc7ae827
User <User@LAB3-3>
2024-12-18 15:17:00 +0200

    add parts and image need to do the func

--- TheGarageManagerServer/DTO/GaragePartsDTO.cs
+++ TheGarageManagerServer/DTO/GaragePartsDTO.cs
@@ -3,12 +3,38 @@
     public class GaragePartsDTO
     {
         public int PartID { get; set; }
-        public int GarageID { get; set; }
-        public string PartName { get; set; }
-        public int PartNumber { get; set; }
-        public int Cost { get; set; }
-        public string ImageURL { get; set; }
+        public int? GarageID { get; set; }
+        public string? PartName { get; set; }
+        public int? PartNumber { get; set; }
+        public int? Cost { get; set; }
+        public string? ImageURL { get; set; }
 
+
+        public GaragePartsDTO() { }
+
+        public GaragePartsDTO(Models.GaragePart modelParts)
+        {
+            this.PartID = modelParts.PartId;
+            this.GarageID = modelParts.GarageId;
+            this.PartName = modelParts.PartName;
+            this.PartNumber = modelParts.PartNumber;
+            this.Cost = modelParts.Cost;
+            this.ImageURL = modelParts.ImageUrl;
+        }
+
+        public Models.GaragePart GetGarageParts()
+        {
+            Models.GaragePart modelParts = new Models.GaragePart()
+            {
+                PartId = this.PartID,
+                GarageId = this.GarageID,
+                PartName = this.PartName,
+                PartNumber = this.PartNumber,
+                Cost = this.Cost,
+                ImageUrl = this.ImageURL
+            };
+            return modelParts;
+        }
 
     }
 }
