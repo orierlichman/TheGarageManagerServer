2f4fe1a249fad157d110cf6da1ddd44efd88855b
User <User@LAB3-3>
2025-01-22 16:17:55 +0200

    do

--- TheGarageManagerServer/DTO/VehicleDTO.cs
+++ TheGarageManagerServer/DTO/VehicleDTO.cs
@@ -3,11 +3,44 @@
     public class VehicleDTO
     {
         public string LicensePlate { get; set; }  
-        public string Model { get; set; }
-        public int YearVehicle { get; set; }
-        public string FuelType { get; set; }
-        public string Color { get; set; }
-        public string Manufacturer { get; set; }
-        public int CurrentMileage { get; set; } 
-    }
+        public string? Model { get; set; }
+        public int? YearVehicle { get; set; }
+        public string? FuelType { get; set; }
+        public string? Color { get; set; }
+        public string? Manufacturer { get; set; }
+        public int? CurrentMileage { get; set; }
+
+        public VehicleDTO() { }
+
+        public VehicleDTO(Models.Vehicle modelVehicle)
+        {
+            this.LicensePlate = modelVehicle.LicensePlate;
+            this.Model = modelVehicle.Model;
+            this.YearVehicle = modelVehicle.YearVehicle;
+            this.FuelType = modelVehicle.FuelType;
+            this.Color = modelVehicle.Color;
+            this.Manufacturer = modelVehicle.Manufacturer;
+            this.CurrentMileage = modelVehicle.CurrentMileage;
+        }
+
+
+        public Models.Vehicle GetVehicle()
+        {
+            Models.Vehicle modelVehicle = new Models.Vehicle()
+            {
+                LicensePlate = this.LicensePlate,
+                Model = this.Model,
+                YearVehicle = this.YearVehicle,
+                FuelType = this.FuelType,
+                Color = this.Color,
+                Manufacturer = this.Manufacturer,
+                CurrentMileage = this.CurrentMileage
+            };
+            return modelVehicle;
+        }
+
+
+
+
+     }
 }
